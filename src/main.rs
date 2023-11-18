2e669c97cf72d0320d8d0becc1227baa453f601e
Uxugin <git@uxugin.com>
2023-11-18 12:47:31 -0700

    remove comments and blank lines

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,15 @@
 use gtk::prelude::*;
 use gtk::{glib, Application, ApplicationWindow};
-
 const APP_ID: &str = "org.Snowbotics39131.mission_creator";
-
 fn main() -> glib::ExitCode {
-    // Create a new application
     let app = Application::builder().application_id(APP_ID).build();
-
-    // Connect to "activate" signal of `app`
     app.connect_activate(build_ui);
-
-    // Run the application
     app.run()
 }
-
 fn build_ui(app: &Application) {
-    // Create a window and set the title
     let window = ApplicationWindow::builder()
         .application(app)
         .title("Snowbotics Mission Creator")
         .build();
-
-    // Present window
     window.present();
 }
