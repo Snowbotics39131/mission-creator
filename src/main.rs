05da0a2e4a2f5bdd7089271d7a8a2234e89ef503
Uxugin <git@uxugin.com>
2023-11-18 20:08:58 -0700

    add example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow};
+use gtk::{self, glib, Application, ApplicationWindow, Orientation, Button, Label};
 const APP_ID: &str = "org.Snowbotics39131.mission_creator";
 fn main() -> glib::ExitCode {
     let app = Application::builder().application_id(APP_ID).build();
@@ -7,9 +7,37 @@ fn main() -> glib::ExitCode {
     app.run()
 }
 fn build_ui(app: &Application) {
+    let hor_box = gtk::Box::builder()
+        .orientation(Orientation::Horizontal)
+        .build();
+    let add_button = Button::builder()
+        .label("+")
+        .build();
+    let action_label = Label::builder()
+        .label("ExampleAction")
+        .build();
+    let up_down_box = gtk::Box::builder()
+        .orientation(Orientation::Vertical)
+        .build();
+    let up_button = Button::builder()
+        .label("^")
+        .build();
+    let down_button = Button::builder()
+        .label("v")
+        .build();
+    let delete_button = Button::builder()
+        .label("X")
+        .build();
+    up_down_box.append(&up_button);
+    up_down_box.append(&down_button);
+    hor_box.append(&add_button);
+    hor_box.append(&action_label);
+    hor_box.append(&up_down_box);
+    hor_box.append(&delete_button);
     let window = ApplicationWindow::builder()
         .application(app)
         .title("Snowbotics Mission Creator")
+        .child(&hor_box)
         .build();
     window.present();
 }
