e7d32814499a6f651bd9fc2e7f8282fdcca5d9c6
Uxugin <git@uxugin.com>
2023-11-19 12:37:13 -0700

    change version, update readme, make action a dropdown, add currently non-functional add and compile buttons

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-This crate is in very early stages of development and is not currently functional. You should probably not download it unless you are on the Snowbotics team.
+This crate is in very early stages of development and is not currently functional. You should probably not download it yet unless you are on the Snowbotics team.
 
 Graphically create missions for Snowbotics Framework.
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use gtk::prelude::*;
-use gtk::{self, glib, Application, ApplicationWindow, Orientation, Button, Label, Arrow};
+use gtk::{self, glib, Application, ApplicationWindow, Orientation, Button, DropDown};
 const APP_ID: &str = "org.Snowbotics39131.mission_creator";
 fn main() -> glib::ExitCode {
     let app = Application::builder().application_id(APP_ID).build();
@@ -7,41 +7,48 @@ fn main() -> glib::ExitCode {
     app.run()
 }
 fn build_ui(app: &Application) {
-    let hor_box = gtk::Box::builder()
+    let actions = ["DriveStraightAction", "DriveTurnAction", "DriveCurveAction"];
+    let main_box = gtk::Box::builder()
+        .orientation(Orientation::Vertical)
+        .build();
+    let compile_button = Button::builder()
+        .label("Compile to Python")
+        .build();
+    let action_box = gtk::Box::builder()
         .orientation(Orientation::Horizontal)
         .build();
     let add_button = Button::builder()
         .label("+")
         .build();
-    let action_label = Label::builder()
-        .label("ExampleAction")
-        .build();
+    let action_label = DropDown::from_strings(&actions.as_slice());
     let up_down_box = gtk::Box::builder()
         .orientation(Orientation::Vertical)
         .build();
-    /*let up_button = Button::builder()
+    let up_button = Button::builder()
         .label("^")
         .build();
     let down_button = Button::builder()
         .label("v")
-        .build();*/
-    let up_button = Arrow::new()
-        .build();
-    let down_button = Arrow::new()
         .build();
     let delete_button = Button::builder()
         .label("X")
         .build();
+    let add_action_button = Button::builder()
+        .label("+")
+        .build();
     up_down_box.append(&up_button);
     up_down_box.append(&down_button);
-    hor_box.append(&add_button);
-    hor_box.append(&action_label);
-    hor_box.append(&up_down_box);
-    hor_box.append(&delete_button);
+    action_box.append(&add_button);
+    action_box.append(&action_label);
+    action_box.append(&up_down_box);
+    action_box.append(&delete_button);
+    main_box.append(&compile_button);
+    main_box.append(&action_box);
+    main_box.append(&add_action_button);
     let window = ApplicationWindow::builder()
         .application(app)
         .title("Snowbotics Mission Creator")
-        .child(&hor_box)
+        .child(&main_box)
         .build();
     window.present();
 }
