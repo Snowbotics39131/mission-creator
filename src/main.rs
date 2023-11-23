6ee6c5453e0e83d466744eff39492101c39cb788
Uxugin <git@uxugin.com>
2023-11-23 14:12:37 -0700

    make add_action_button do something

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use gtk::prelude::*;
 use gtk::{self, glib, Application, ApplicationWindow, Orientation, Button};
+use glib::clone;
 mod action_widget;
 const APP_ID: &str = "org.Snowbotics39131.mission_creator";
 fn main() -> glib::ExitCode {
@@ -17,6 +18,9 @@ fn build_ui(app: &Application) {
     let add_action_button = Button::builder()
         .label("+")
         .build();
+    add_action_button.connect_clicked(clone!(@weak main_box => move |_| {
+        main_box.append(&action_widget::ActionWidget::new());
+    }));
     let action_box = action_widget::ActionWidget::new();
     main_box.append(&compile_button);
     main_box.append(&action_box);
